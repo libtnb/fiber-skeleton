33cde69c920499c14c9835017701cd9064844230
耗子 <i@haozi.net>
2024-12-15 02:05:07 +0800

    fix: lint

--- internal/http/request/user.go
+++ internal/http/request/user.go
@@ -5,10 +5,10 @@ type UserID struct {
 }
 
 type AddUser struct {
-	Name string `json:"name" form:"name" validate:"required|min_len:3|max_len:255" comment:"用户名"`
+	Name string `json:"name" form:"name" validate:"required|min_len:3|max_len:255"`
 }
 
 type UpdateUser struct {
 	ID   uint   `uri:"id" validate:"required|number"`
-	Name string `json:"name" form:"name" validate:"required|min_len:3|max_len:255" comment:"用户名"`
+	Name string `json:"name" form:"name" validate:"required|min_len:3|max_len:255"`
 }
