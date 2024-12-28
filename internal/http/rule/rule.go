c2895ae8d6d86365a5bc100e00fd87abd0aeb6fd
耗子 <i@haozi.net>
2024-12-29 01:10:12 +0800

    feat: 优化notExists验证器名称

--- internal/http/rule/rule.go
+++ internal/http/rule/rule.go
@@ -7,11 +7,11 @@ import (
 
 func GlobalRules(db *gorm.DB) {
 	validate.AddValidators(validate.M{
-		"exists":     NewExists(db).Passes,
-		"not_exists": NewNotExists(db).Passes,
+		"exists":    NewExists(db).Passes,
+		"notExists": NewNotExists(db).Passes,
 	})
 	validate.AddGlobalMessages(map[string]string{
-		"exists":     "{field} 不存在",
-		"not_exists": "{field} 已存在",
+		"exists":    "{field} 不存在",
+		"notExists": "{field} 已存在",
 	})
 }
